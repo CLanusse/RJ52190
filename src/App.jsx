f1fe54ff00eaf57fc7249c623bc0cf117279c83d
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-04-10 22:37:50 -0300

    clase 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Equipo from "./Equipo.jsx"
 
 function App() {
+  const profe = "Rodrigo Maestre";
+
+  const calculo = (n) => {
+    return 2 + 2 * n - 30;
+  };
+
+  const estilos = {
+    marginLeft: "40px",
+    backgroundColor: "green",
+    color: "white",
+  };
 
   return (
     <div className="App">
@@ -14,18 +26,24 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>RJ 52190</h1>
+
+      <h1
+        style={estilos}
+      >
+        El profesor es: {profe}{" "}
+      </h1>
       <h4>Bienvenidos a React JS con Vite</h4>
       <div className="card">
-        <p>
-          Conrado Lanusse el mejor del mundo
-        </p>
+        <p>{calculo(125)}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Equipo />
+      <Equipo />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Equipo.jsx
@@ -0,0 +1,19 @@
+
+
+
+const Equipo = () => {
+
+    return (
+        <ul>
+            <h2>Equipo</h2>
+            <hr/>
+            <li>Conrado Lanusse</li>
+            <li>Rodrigo Maestre</li>
+            <li>Cristian Ovejero</li>
+
+            <div>Coderhouse 2023</div>
+        </ul>
+    )
+}
+
+export default Equipo
