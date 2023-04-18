0c57363dff850e028fd8887d40589ab51eaf587a
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-04-17 22:41:27 -0300

    Componentes 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { Navbar } from "./components/Navbar/Navbar";
+import TextContainer from './ejemplos/Contenedor';
+import { Clicker } from './ejemplos/Clicker';
 import { Button } from 'react-bootstrap';
+import Parrafos from './ejemplos/Parrafos';
+import { useState } from 'react';
 
 function App() {
+
+  const [show, setShow] = useState(true)
+
+  const handleShow = () => {
+    setShow(!show)
+  }
 
   return (
     <div>
       <Navbar />
-      <ItemListContainer saludo={"Hola mundo!"}/>
+      {/* <ItemListContainer saludo={"Hola mundo!"}/> */}
+      {/* <Parrafos /> */}
 
-      {/* <button className="btn btn-primary">Click me</button> */}
-      <Button variant="success">Click me 2</Button>
-      <Button variant="outline-danger" className='mx-3'>Click me 3</Button>
+      <button className='btn btn-primary' onClick={handleShow}>Mostrar clicker</button>
+
+      {
+        show ? <Clicker/> : <h3>Clicker oculto</h3>
+      }
+      
     </div>
   );
 }
--- src/ejemplos/Clicker.jsx
+++ src/ejemplos/Clicker.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 
 
 export const Clicker = () => {
-    console.log("Clicker actualizado")
+
     const [counter, setCounter] = useState(10)
     const [saludo, setSaludo] = useState("Hola")
+    // const [carrito, setCarrito] = useState([])
+
+    const dateRef = useRef(new Date())
 
     const sumar = () => {
         setCounter( counter + 1 )
@@ -14,10 +17,40 @@ export const Clicker = () => {
         setSaludo(saludo + " hola")
     }
 
+    
+
+    useEffect(() => {
+        console.log("Componente montado")
+
+        // fetch('https://jsonplaceholder.typicode.com/posts/1')
+        //     .then((response) => response.json())
+        //     .then((json) => console.log(json));
+
+        return () => {
+            // funcion de limpieza || cleanup effect
+            console.log("COmponente desmontado")
+        }
+    }, [])
+
+    // useEffect(() => {
+    //     console.log("Esto va después del render")
+    //     // dateRef.current = new Date()   
+        
+    //     return () => {
+    //         console.log("Esto va antes del render")
+    //     }
+    // }, [saludo, counter])
+
+    // useEffect(() => {
+    //     localStorage.setItem('carrito', JSON.stringify(carrito))
+    // }, [carrito])
+
+
     return (
-        <div>
-            <button onClick={sumar}>Click me</button>
+        <div className="container my-5">
+            <button onClick={sumar} className="btn btn-primary">Click me</button>
             <p>Contador: {counter}</p>
+            <p>Componente montado: {dateRef.current.toLocaleString()}</p>
 
             <div onClick={agregarSaludo}>
                 {saludo}
